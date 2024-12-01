afef82c2307a7a71993199004ce0c44d778ce028
Phanuwat Phoowichai <taliw@odds.team>
2024-12-01 13:26:03 +0700

    add routes path

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
-  def index
-    
+  def index # this is back end
+    @articles = Article.all
+  end
+  def show 
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
-  
-  get "/articles", to: "articles#index"
+  resources :articles
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to: "articles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,4 +15,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # 
 end
